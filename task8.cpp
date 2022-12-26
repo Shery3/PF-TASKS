9ea45e768c76a06b1fa2c582338040d27edfd768
Shery3 <120157081+Shery3@users.noreply.github.com>
2022-12-26 07:18:03 +0500

    Add files via upload

---
+++ task8.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+main(){
+
+   float megabytes;
+   float mb;
+   float inbits;
+   mb=1024*1024*8;
+
+   cout<<"ENTER MEGABYTES: ";
+   cin>>megabytes;
+   inbits=megabytes*mb;
+   cout<<"MEGABYTES IN BITS IS: "<<inbits;
+  
+
+
+
+
+
+
+
+
+
+}
---
+++ task9.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+main(){
+
+
+   int firstnumber;
+   int secondnumber;
+   int threenumber;
+   int fournumber;
+   int fivenumber;
+   int sixnumber;
+   int sevennumber;
+   int eightnumber;
+   int ninenumber;
+   int tennumber;
+   int add;
+   int mul;
+   int div;
+   int sub;
+int result;
+ cout<<"ENTER YOUR FIRST NUMBER: ";
+ cin>>firstnumber;
+
+ cout<<"ENTER YOUR SECOND NUMBER: ";
+ cin>>secondnumber;
+
+ cout<<"ENTER YOUR THIRD NUMBER: ";
+ cin>>threenumber;
+
+ cout<<"ENTER YOUR FOURTH NUMBER: ";
+ cin>>fournumber;
+
+ cout<<"ENTER YOUR FIFTH NUMBER: ";
+ cin>>fivenumber;
+
+ cout<<"ENTER YOUR SIXTH NUMBER: ";
+ cin>>firstnumber;
+
+ cout<<"ENTER YOUR SEVENTH NUMBER: ";
+ cin>>sevennumber;
+
+ cout<<"ENTER YOUR EIGHTH NUMBER: ";
+ cin>>eightnumber;
+
+ cout<<"ENTER YOUR NINTH NUMBER: ";
+ cin>>ninenumber;
+
+ cout<<"ENTER YOUR TENTH NUMBER: ";
+ cin>>tennumber;
+ 
+ add=threenumber+fivenumber+ninenumber;
+ mul=sevennumber*tennumber*sixnumber;
+ div=eightnumber/fournumber;
+ sub=secondnumber-firstnumber;
+ result=add+mul+div+sub;
+ cout<<"YOUR ADDITION ANSWER: "<<add<<endl;
+ cout<<"YOUR MULTIPLICATION ANSWER: "<<mul<<endl;
+ cout<<"YOUR DIVISION ANSWER: "<<div<<endl;
+ cout<<"YOUR SUBTRACTION ANSWER: "<<sub<<endl;
+ cout<<"result is:"<<result;
+}
